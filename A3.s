66fc59b7aa127683e9b50f313ab58774570e866c
KripeshThapaliya <133162585+KripeshThapaliya@users.noreply.github.com>
2023-07-26 19:46:08 +0000

    A3(10100011) Completed

--- A3.s
+++ A3.s
@@ -1,17 +1,36 @@
 .global _start
 _start:
-    MOV R0, #1
-    LDR R1, =message
-    LDR R2, =13
-    MOV R7, #4 // system call to print to terminal
-    SWI 0
+    MOV R0, #35
+    MOV R1, #14
+    MOV R5, #15
 
-    // exit the program 
-    MOV R0, #0
+    CMP R0, R1  //campare the value in R0 to the value in R1(SUBS R0,R1 without the result )
+    BGT greater //if R0 > R1, branch to the label 'greater'
+    BLT lesser  //If R0 < R1 , branch to the label 'lesser'
+    BEQ equaler  //If R0 = R1 , branch to the label 'equaler'
+
+
+greater:
+    LSL R5, #2 //Logical shift left the value in R5 x2 -> Equivalent to multiplying by 2^2
+
+    //Sequence to terminate the program
+    MOV R0, R5 //Indicates this program executed successfully
     MOV R7, #1
     SWI 0
 
-.section .data
-message: 
-    .asciz "Hello World\n"
+lesser:
+    LSR R5, #1 //Logical shift right the value in R5 -> Equivalent to multiplying by 2^1
+
+        //Sequence to terminate the program
+    MOV R0, R5 //Indicates this program executed successfully
+    MOV R7, #1
+    SWI 0
+
+equaler:
+    ROR R5, #2 //Perform a right rotation on R5
+
+            //Sequence to terminate the program
+    MOV R0, R5 //Indicates this program executed successfully
+    MOV R7, #1
+    SWI 0
     
